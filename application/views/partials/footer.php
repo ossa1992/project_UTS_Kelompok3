5f4be8c904939b7c6459696c30853f2449ebb68d
Ossa F. Ibrahim <82696298+ossa1992@users.noreply.github.com>
2022-06-06 16:50:47 +0700

    Add files via upload

---
+++ application/views/partials/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span>Copyright © <a href="" target="_blank">Ossa fauzi ibrahim, Mayrevsabda cahyanto, Lulu ilkamilah, Fajar hendrawan</a></span>
+		</div>
+	</div>
+</footer>
